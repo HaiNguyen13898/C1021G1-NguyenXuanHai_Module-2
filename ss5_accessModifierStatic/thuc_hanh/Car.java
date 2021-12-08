17c381ae6dac3c7547bd24771a8f6dca52e221bb
HaiNguyen13898 <93204872+HaiNguyen13898@users.noreply.github.com>
2021-12-08 08:00:21 +0700

    nop bai

---
+++ ss5_accessModifierStatic/thuc_hanh/Car.java
@@ -0,0 +1,30 @@
+package accessModifierStatic.thuc_hanh;
+
+public class Car {
+    private String name;
+
+    private String engine;
+
+    public static int numberOfCars;
+
+    public Car(String name, String engine) {
+
+        this.name = name;
+
+        this.engine = engine;
+
+        numberOfCars++;
+
+    }
+    public static void main(String[] args) {
+
+        Car car1 = new Car("Mazda 3","Skyactiv 3");
+
+        System.out.println(car1.name);
+
+        Car car2 = new Car("Mazda 6","Skyactiv 6");
+
+        System.out.println(Car.numberOfCars);
+
+    }
+}
