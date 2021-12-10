1f69098d78dc5e8654d7c0a20ff53f625ce433d2
HaiNguyen13898 <93204872+HaiNguyen13898@users.noreply.github.com>
2021-12-10 13:53:15 +0700

    nop bai

---
+++ ss6_ke_thua/bai_tap/circle_cylinder/Test.java
@@ -0,0 +1,13 @@
+package kethua.bai_tap.circle_cylinder;
+
+public class Test {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        circle.setRadius(5);
+        System.out.println("Area circle :" + circle.getArea());
+
+        Cylinder cylinder = new Cylinder(circle.getRadius(), circle.getColor(), 10);
+        System.out.println("Total is: " + cylinder.getTotalArea());
+
+    }
+}
---
+++ ss6_ke_thua/bai_tap/point_MoveablePoint/Test.java
@@ -0,0 +1,27 @@
+package kethua.bai_tap.point_MoveablePoint;
+
+public class Test {
+    public static void main(String[] args) {
+        Point point = new Point();
+
+        point.setX(0);
+        point.setY(3);
+        System.out.println("Point : " + point.toString());
+
+        point.setXY(0, 4);
+        System.out.println("Point : " + point.toString());
+
+        MovablePoint movablePoint = new MovablePoint();
+
+        movablePoint.setX(point.getX());
+        movablePoint.setY(point.getY());
+
+        movablePoint.setxSpeed(3);
+        movablePoint.setySpeed(2);
+
+        movablePoint.move();
+
+        System.out.println(movablePoint.toString());
+
+    }
+}
